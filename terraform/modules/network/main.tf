1902c04a4a48f9484342f6db2744121fc77bfd51
Rattipong Sakunjeen <rattipong.mark@gmail.com>
2026-04-23 22:48:00 +0700

    fix: upgrade public ip to standard sku

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -19,8 +19,8 @@ resource "azurerm_public_ip" "pip" {
   name                = "hero-public-ip"
   location            = var.location
   resource_group_name = var.resource_group_name
-  allocation_method   = "Dynamic"
-  sku                 = "Basic"
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_network_security_group" "nsg" {
