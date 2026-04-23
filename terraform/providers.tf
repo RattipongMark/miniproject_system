e10cfcb5201d32e4e268229438c72e4c430c4cc6
Rattipong Sakunjeen <rattipong.mark@gmail.com>
2026-04-23 22:59:24 +0700

    feat: implement remote backend for state management

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstatemakk1776959829"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
