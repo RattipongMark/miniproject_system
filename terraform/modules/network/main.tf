b57ead0df3bb6ce57dd74769dfd45938c1ec15b1
Rattipong Sakunjeen <rattipong.mark@gmail.com>
2026-04-24 01:15:33 +0700

    feat: use start-first with wget healthcheck

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -53,6 +53,11 @@ resource "azurerm_network_security_group" "nsg" {
   }
 }
 
+resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
 output "subnet_id" { value = azurerm_subnet.subnet.id }
 output "public_ip_id" { value = azurerm_public_ip.pip.id }
 output "public_ip_address" { value = azurerm_public_ip.pip.ip_address }
